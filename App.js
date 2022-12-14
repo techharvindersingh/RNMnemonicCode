01c0f37bc1ad1be98484f9e1a1671459bfde40f4
iqbalsingh108 <37610299+Neeraj-Immanent@users.noreply.github.com>
2022-12-14 11:27:55 +0530

    Initial Commit Mnemonic Code Practice

---
+++ App.js
@@ -0,0 +1,39 @@
+import * as React from 'react';
+import {StyleSheet, View, Text} from 'react-native';
+import {RNMnemonicKey} from 'react-native-mnemonic-key';
+
+export default function App() {
+  const [result, setResult] = React.useState(undefined);
+
+  React.useEffect(() => {
+    RNMnemonicKey.create({
+      mnemonic:
+        'satisfy adjust timber high purchase tuition stool faith fine install that you unaware feed domain license impose boss human eager hat rent enjoy dawn',
+      coinType: 118,
+      account: 5,
+      index: 32,
+    }).then(resp => console.log(resp));
+  }, []);
+
+  return (
+    <View style={styles.container}>
+      <Text>Address: {result?.accAddress}</Text>
+      <Text>ValAddress: {result?.valAddress}</Text>
+      <Text>Mnemonic: {result?.mnemonic}</Text>
+      <Text>PrivateKey: {result?.privateKey.toString('hex')}</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  box: {
+    width: 60,
+    height: 60,
+    marginVertical: 20,
+  },
+});
---
+++ ios/File.swift
@@ -0,0 +1,8 @@
+//
+//  File.swift
+//  mnemenic_sample
+//
+//  Created by IOS on 02/08/21.
+//
+
+import Foundation
